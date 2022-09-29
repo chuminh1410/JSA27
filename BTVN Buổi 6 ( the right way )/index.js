00fb476370a34bb20703ed1a7e9a93bdc5eb6c07
chuminh1410 <chungocquangminh@gmail.com>
2022-09-29 09:05:28 +0700

    up lan n

---
+++ BTVN Buổi 6 ( the right way )/index.js
@@ -0,0 +1,25 @@
+// Tạo 1 var có 3 biến : Táo, Chuối và Xoài
+var gioHang = {
+    apple:0,
+    melon:0,
+    mango:0 ,
+}
+
+// Làm function cho button 
+function clickME1() {
+    gioHang.apple ++
+    console.log(gioHang)
+}
+
+function clickME2() {
+    gioHang.melon ++
+    console.log(gioHang)
+}
+
+function clickME3() {
+    gioHang.mango ++
+    console.log(gioHang)
+}
+
+
+var myString = JSON.stringify()
