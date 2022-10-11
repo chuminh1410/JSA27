d3c2fd5317cd2e09cbd981dcf8c27cbd129f0cc9
chuminh1410 <chungocquangminh@gmail.com>
2022-10-11 15:25:17 +0700

    up

---
+++ Buổi 7/index.js
@@ -0,0 +1,25 @@
+/*
+* JS next gen
+* ES6
+*
+*
+* let
+* arrow function
+*
+*/
+
+
+
+{
+    let a = 10
+    console.log(a)
+}
+
+function xuatChu(){
+    let a = "toi ten chu"
+    console.log(a)
+}
+
+let b = "ai do"{
+    console.log b 
+}
